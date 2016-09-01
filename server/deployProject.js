ec1809b8d1aa29d07d19e7fda04d3e078a1883d1
Ashok Kumar Venkatesh <28ash92@gmail.com>
2016-09-01 05:34:42 +0000

    Env path is configurable now

--- server/deployProject.js
+++ server/deployProject.js
@@ -1,18 +1,11 @@
 var path = require('path');
 
-<<<<<<< HEAD
+
 var deployProject = function(currentDirectoryPath) {
 
       const spawn = require('child_process').spawn;
       
       console.log("looking for docker file in ",currentDirectoryPath);
-=======
-var deployProject = function(parentDir) {
-
-      const spawn = require('child_process').spawn;
-
-      console.log(parentDir);
->>>>>>> 3d457fb8f984379c9564b02782ad502662d7cf2a
 
       var dockerComposeCommand = spawn('docker-compose', ['up','-d'], {cwd : currentDirectoryPath});
         
